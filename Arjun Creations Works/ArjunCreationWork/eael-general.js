5db4eb1e90969d3a4defb38fe16c11defced8a10
Durga Prasad Vaddepalli <118739789+durgaprasadsagar@users.noreply.github.com>
2025-01-29 15:05:36 +0530

    Add files via upload

---
+++ Arjun Creations Works/ArjunCreationWork/eael-general.js
@@ -0,0 +1,65 @@
+/* <![CDATA[ */
+var localize = {
+    "ajaxurl": "https:\/\/Arjun Creations & Works\/wp-admin\/admin-ajax.php",
+    "nonce": "0c3a5aaf1d",
+    "i18n": {
+        "added": "Added ",
+        "compare": "Compare",
+        "loading": "Loading..."
+    },
+    "eael_translate_text": {
+        "required_text": "is a required field",
+        "invalid_text": "Invalid",
+        "billing_text": "Billing",
+        "shipping_text": "Shipping",
+        "fg_mfp_counter_text": "of"
+    },
+    "page_permalink": "https:\/\/Arjun Creations & Works\/",
+    "cart_redirectition": "",
+    "cart_page_url": "",
+    "el_breakpoints": {
+        "mobile": {
+            "label": "Mobile Portrait",
+            "value": 767,
+            "default_value": 767,
+            "direction": "max",
+            "is_enabled": true
+        },
+        "mobile_extra": {
+            "label": "Mobile Landscape",
+            "value": 880,
+            "default_value": 880,
+            "direction": "max",
+            "is_enabled": true
+        },
+        "tablet": {
+            "label": "Tablet Portrait",
+            "value": 1024,
+            "default_value": 1024,
+            "direction": "max",
+            "is_enabled": true
+        },
+        "tablet_extra": {
+            "label": "Tablet Landscape",
+            "value": 1200,
+            "default_value": 1200,
+            "direction": "max",
+            "is_enabled": true
+        },
+        "laptop": {
+            "label": "Laptop",
+            "value": 1366,
+            "default_value": 1366,
+            "direction": "max",
+            "is_enabled": true
+        },
+        "widescreen": {
+            "label": "Widescreen",
+            "value": 2400,
+            "default_value": 2400,
+            "direction": "min",
+            "is_enabled": true
+        }
+    }
+};
+/* ]]> */
---
+++ Arjun Creations Works/ArjunCreationWork/simple-likes-public.js
@@ -0,0 +1,6 @@
+/* simple-likes-public.js */
+var simpleLikes = {
+    ajaxurl: "https://Arjun Creations & Works/wp-admin/admin-ajax.php",
+    like: "Like",
+    unlike: "Unlike"
+};
